eb41e7a4e0c1e3b84b31a3edcf68366b54039f07
Fang <>
2022-11-13 18:04:36 +0800

    --wip-- [skip ci]

--- pancake.py
+++ pancake.py
@@ -2,6 +2,7 @@ from uniswap import Uniswap
 import ccxt
 import logging
 import time
+import json
 
 address = None         # or None if you're not going to make transactions
 private_key = None  # or None if you're not going to make transactions
@@ -12,24 +13,23 @@ binance = ccxt.binance()
 binance.load_markets()
 
 logging.basicConfig(level=logging.ERROR)
+tokens = json.load(open('token.json'))
+print(tokens)
 
-# Some token addresses we'll be using later in this guide
-ETH = "0x0000000000000000000000000000000000000000"
-BNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
 BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"
-LUNA = "0x156ab3346823B651294766e23e6Cf87254d68962"
 USDT = "0x55d398326f99059fF775485246999027B3197955"
-YFII = "0x7F70642d88cf1C4a3a7abb072B53B929b653edA5"
+
+BASE = "USDT"
 
 while True:
     time.sleep(5)
-    SYMBOL = "BNB"
-    order_book = binance.fetch_order_book(SYMBOL + "/USDT")
-    bid = order_book['bids'][0][0]
-    price = uniswap.get_price_input(BNB, USDT, 10**18)
-    print("BNB:", BNB)
-    print("pancake price ", ":", price/10**18)
-    print("binance price ", ":", bid)
-    print("------")
+    for token in tokens:
+        order_book = binance.fetch_order_book(token["symbol"] + "/" + BASE)
+        bid = order_book['bids'][0][0]
+        price = uniswap.get_price_input(token["contract"], USDT, 10**18)
+        print(token["symbol"], ":", token["contract"])
+        print("pancake price ", ":", price/10**18)
+        print("binance price ", ":", bid)
+        print("------")
 
 
