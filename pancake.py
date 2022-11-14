7cae9020bc767a49996eac0d73f59aa14744f6fb
Fang <>
2022-11-14 14:21:06 +0800

    --wip-- [skip ci]

--- pancake.py
+++ pancake.py
@@ -1,4 +1,5 @@
 from uniswap import Uniswap
+from web3 import Web3
 import ccxt
 import logging
 import time
@@ -9,12 +10,12 @@ private_key = None  # or None if you're not going to make transactions
 version = 2                       # specify which version of Uniswap to use
 provider = "https://bsc-dataseed.binance.org/"    # can also be set through the environment variable `PROVIDER`
 uniswap = Uniswap(address=address, private_key=private_key, version=version, provider=provider)
+print("init...")
 binance = ccxt.binance()
 binance.load_markets()
 
 logging.basicConfig(level=logging.ERROR)
 tokens = json.load(open('token.json'))
-print(tokens)
 
 BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"
 USDT = "0x55d398326f99059fF775485246999027B3197955"
@@ -24,16 +25,33 @@ BASE = "USDT"
 while True:
     try:
         for token in tokens:
-            order_book = binance.fetch_order_book(token["symbol"] + "/" + BASE)
-            bid = order_book['bids'][0][0]
-            price = uniswap.get_price_input(token["contract"], USDT, 10**token["decimals"])
+            #print("route :", token["route"])
+            price = uniswap.get_price_input(
+                Web3.toChecksumAddress(token["contract"]), 
+                USDT, 
+                10**token["decimals"],
+                None,
+                token["route"])
             print(token["symbol"], ":", token["contract"])
+
+            try:
+                order_book = binance.fetch_order_book(token["symbol"] + "/" + BASE)
+            except Exception as e:
+                print(e)
+                pass
+                continue
+            bid = order_book['bids'][0][0]
+
             print("pancake price ", ":", price/(10**18))
             print("binance price ", ":", bid)
-            print("diff", ":",  "%.2f" %(100 * ((price/(10**18) - bid)/bid)), "%")
+            diff = 100 * ((price/(10**18) - bid)/bid)
+            print("diff", ":",  "%.2f" %diff, "%")
+            if abs(diff) > 2:
+                print("😱 diff is big!")
             print(" ")
         print("------")
     except Exception as e:
+        print(e)
         pass
     time.sleep(5)
 
